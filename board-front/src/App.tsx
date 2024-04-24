af9cff83de688cb15ec6d20cb3fac18bca4112d8
koreaBean <ksg04001@daum.net>
2024-04-24 13:16:49 +0900

    서타몽 - 동영상 17 끝 ( Footer 작업)

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,21 +1,13 @@
-import BoardItem from "components/BoardItem";
-import CommentItem from "components/CommentItem";
-import FavoriteItem from "components/FavoriteItem";
-import InputBox from "components/InputBox";
-import Top3item from "components/Top3item";
-import { latestBoardListMock, top3boardListMock,commentListMock,FavoriteListMock } from "mocks";
 import 'App.css';
-import { useState } from "react";
+import Footer from 'layouts/Footer';
 
 
 function App() {
 
-const [value, setValue] = useState<string>('');
 
   return (
     <>
-
-    <InputBox/>
+    <Footer/>
     </>
   )
 }
--- board-front/src/components/InputBox/index.tsx
+++ board-front/src/components/InputBox/index.tsx
@@ -1,4 +1,4 @@
-import {ChangeEvent, Dispatch,SetStateAction,forwardRef} from 'react'
+import {KeyboardEvent,ChangeEvent, Dispatch,SetStateAction,forwardRef} from 'react'
 import './style.css';
 
 interface Props {
@@ -9,35 +9,49 @@ interface Props {
   setValue : Dispatch<SetStateAction<string>>
   error : boolean;
 
+  // icon이 없으면 null 이 나올 수 있음. 필수가 아닌 값은 ? 붙여주기.
+  icon? : string; 
+
+  onButtonClick? :() => void;
+
+  message? : string;
+
+  onKeyDown? : (event : KeyboardEvent<HTMLInputElement>) => void;
+
 }
 
-//          component : InputBox 컴포넌트
+//          component : InputBox 컴포넌트               //
 const InputBox = forwardRef<HTMLInputElement,Props>((props : Props,ref) => {
 
-const {label, type,error, placeholder, value, setValue} = props;
+const {label, type,error, placeholder, value, setValue, onButtonClick, icon, message, onKeyDown} = props;
 
-const onChangeHandler = (event : ChangeEvent<HTMLInputElement) => {
+const onChangeHandler = (event : ChangeEvent<HTMLInputElement>) => {
 
   const value = event.target.value
   setValue(value);
 }
 
-
+//          event handler : input 키 이벤트 처리 함수       //
+const onKeyDownHandler = (event : KeyboardEvent<HTMLInputElement>) => {
+  if(!onKeyDown) return;
+  onKeyDown(event);
+} 
 
 
   return (
     <div className='inputbox'>
       <div className='inputbox-lable'>{label}</div>
       <div className={error ? 'inputbox-container-error' : 'inputbox-container'}>
-        <input type={type} className='input'placeholder={placeholder} value={value} onChange={onChangeHandler} />
-        <div className='icon-button'>
-          <div className='icon eye-light-off-icon'></div>
-        </div>
+        <input ref={ref} type={type} className='input'placeholder={placeholder} value={value} onChange={onChangeHandler} onKeyDown={onKeyDownHandler} />
+        {onButtonClick !== undefined && (
+          <div className='icon-button'>
+            {icon !== undefined && (<div className={`icon ${icon}`}></div>)}
+          </div>
+        )}
       </div>
-      <div className='inputbox-message'>{'비밀번호를 8자이상 입력해주세요.'}</div>
-
+      {message !== undefined && <div className='inputbox-message'>{message}</div>}
     </div>
   )
-})
+});
 
 export default InputBox;
